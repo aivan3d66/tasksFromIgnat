c17d00857adedc4690aa9214a6dd6ed277b7dc8c
Ivan <adamovskij00@mail.ru>
2021-12-08 21:32:17 +0300

    add props typing

--- my-app/src/p2-homeworks/h1/MessagesList.tsx
+++ my-app/src/p2-homeworks/h1/MessagesList.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Message from "./Message";
+import {MessageItemType} from "../../p1-main/m1-ui/u1-app/redux/reducer/DialogReducer";
+
 const style = require('./Message.module.css');
 
-function MessageList(props: any) {
+type DialogPage = {
+  messages: Array<MessageItemType>,
+  newMessageText: string,
+}
+
+type MessageListProps = {
+  dialogPage: DialogPage,
+  sendMessageCreator: () => void,
+  updateNewMessageTextCreator: (body: string) => void,
+}
+
+function MessageList(props: MessageListProps) {
 
   const state = props.dialogPage.messages;
-  const newMessageBody = state.newMessageText;
+  const newMessageBody = props.dialogPage.newMessageText;
 
   const onSendMessageClick = () => {
     props.sendMessageCreator();
@@ -15,10 +28,12 @@ function MessageList(props: any) {
     props.updateNewMessageTextCreator(body);
   }
 
-  const messagesItems = state.map((m: any) => <Message avatar={m.avatar}
-                                                                name={m.name}
-                                                                message={m.message}
-                                                                time={m.time}/>);
+  const messagesItems = state.map((m: MessageItemType, index: number) => <Message avatar={m.avatar}
+                                                                                  name={m.name}
+                                                                                  message={m.message}
+                                                                                  key={index}
+                                                                                  time={m.time}
+                                                                                  id={m.id}/>);
   return (
     <div>
       <div>
@@ -38,7 +53,7 @@ function MessageList(props: any) {
 
     </div>
 
-)
+  )
 }
 
 export default MessageList;
