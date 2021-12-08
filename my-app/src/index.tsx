adf002059f12b52b6f2b7b0137bd90db5b7a7552
Ivan <adamovskij00@mail.ru>
2021-12-08 20:40:38 +0300

    add BrowserRouter with Provider, import store

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux';
+import {BrowserRouter} from "react-router-dom";
 import './index.css'
 import App from './p1-main/m1-ui/u1-app/App'
+import store from './p1-main/m1-ui/u1-app/redux/redux-store';
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App/>
+        </Provider>
+      </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
