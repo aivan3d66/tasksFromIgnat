60f288c7a28ca18a60e33e3a6e23abe571cb275f
Ivan <adamovskij00@mail.ru>
2021-12-14 22:16:53 +0300

    add typing

--- my-app/src/p2-homeworks/h2/Affair.tsx
+++ my-app/src/p2-homeworks/h2/Affair.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
+import {AffairType} from "./HW2";
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+  // key не нужно типизировать
+  affair:  AffairType// need to fix any
+  deleteAffairCallback: (_id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+  const deleteCallback = () => {
+    props.deleteAffairCallback(props.affair._id)
+  }// need to fix
 
-    return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
-        </div>
-    )
+  return (
+    <div>
+      {props.affair.name}
+      <button onClick={deleteCallback}>X</button>
+    </div>
+  )
 }
 
 export default Affair
