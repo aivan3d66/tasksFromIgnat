0b5043ca53c8951057cc23ce12ee212f23a1a23f
Ivan <adamovskij00@mail.ru>
2021-12-15 21:41:25 +0300

    add @types/uuid dependencies

--- my-app/src/p2-homeworks/h3/HW3.tsx
+++ my-app/src/p2-homeworks/h3/HW3.tsx
@@ -1,34 +1,37 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v4 as uuidv4} from 'uuid';
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+  _id: any // need to fix any
+  name: any // need to fix any
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+  const [users, setUsers] = useState<any>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
-    }
+  const addUserCallback = (name: any) => { // need to fix any
+    setUsers([{
+      // _id: uuidv4(),
+    }]) // need to fix
+  }
 
-    return (
-        <div>
-            <hr/>
-            homeworks 3
+  return (
+    <div>
+      <hr/>
+      homeworks 3
 
-            {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+      {/*should work (должно работать)*/}
+      <GreetingContainer users={users} addUserCallback={addUserCallback}/>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeGreeting/>*/}
-            <hr/>
-        </div>
-    )
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeGreeting/>*/}
+      <hr/>
+    </div>
+  )
 }
 
 export default HW3
