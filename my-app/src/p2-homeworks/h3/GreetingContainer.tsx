9ceaf14e4a7542228aa1c2d33e48e7493141120d
Ivan <adamovskij00@mail.ru>
2021-12-16 11:07:26 +0300

    add GreetingContainerPropsType and setName, setError types

--- my-app/src/p2-homeworks/h3/GreetingContainer.tsx
+++ my-app/src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,10 @@
 import React, {useState, MouseEvent} from 'react'
 import Greeting from './Greeting'
+import {AddUserCallback, UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
-  users: any // need to fix any
-  addUserCallback: any // need to fix any
+  users: Array<UserType> // need to fix any
+  addUserCallback: AddUserCallback // need to fix any
 }
 
 // более простой и понятный для новичков
@@ -12,8 +13,8 @@ type GreetingContainerPropsType = {
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-  const [name, setName] = useState<any>('') // need to fix any
-  const [error, setError] = useState<any>(false) // need to fix any
+  const [name, setName] = useState<string>('') // need to fix any
+  const [error, setError] = useState<boolean>(false) // need to fix any
 
   const setNameCallback = (e: MouseEvent<HTMLButtonElement>) => { // need to fix any
     let val = e.currentTarget.value;
