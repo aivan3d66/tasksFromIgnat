6e4b1ff09d93af424fa9e7d8feeb16d61e25159e
Ivan <adamovskij00@mail.ru>
2021-12-16 20:23:49 +0300

    add setNameCallback typing

--- my-app/src/p2-homeworks/h3/Greeting.tsx
+++ my-app/src/p2-homeworks/h3/Greeting.tsx
@@ -15,12 +15,13 @@ const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
     const inputClass = error ? s.error : "" // need to fix with (?:)
+    const isDisabled = !!error;
 
     return (
         <div className={s.inputForm}>
             <input value={name} onChange={setNameCallback} className={inputClass}/>
             <span className={s.inputErrorText}>{error}</span>
-            <button onClick={addUser} className={s.inputButton}>add</button>
+            <button onClick={addUser} className={s.inputButton} disabled={isDisabled}>add</button>
             <span className={s.inputUsersCount}>Total users: {totalUsers}</span>
         </div>
     )
