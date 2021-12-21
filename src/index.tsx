51383c175f9bb8f226ed3a8abe53f0cfec9c6fa8
Ivan <adamovskij00@mail.ru>
2021-12-21 21:19:22 +0300

    add substitute BrowserRouter by HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import './index.css'
 import App from './p1-main/m1-ui/u1-app/App'
 import store from './p1-main/m1-ui/u1-app/redux/redux-store';
@@ -9,11 +9,11 @@ import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <Provider store={store}>
           <App/>
         </Provider>
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>,
     document.getElementById("root")
 )
