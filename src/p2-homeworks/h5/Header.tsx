2a0fcc695ffc5edb888f21eb2c711c2cdbbf54e4
Ivan <adamovskij00@mail.ru>
2021-12-21 22:51:04 +0300

    add SuperBtn import and ternary operator

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,19 +1,41 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom';
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton';
 import {PATH} from "./Routes";
+import {OnBtnHandler} from "./HW5";
 
 const s = require('./HW5.module.css');
 
-function Header() {
+type HeaderProps = {
+  menu: boolean,
+  onBtnHandler: OnBtnHandler
+}
+
+
+const Header: React.FC<HeaderProps> = ({menu, onBtnHandler}) => {
+  const hideMenuClass = `${menu ? s.hide : s.navList} ${s.navList}`
+
   return (
-    <nav className={s.navList}>
-      <NavLink className={({isActive}) => `${s.nav__item} ${isActive ? s.nav__itemActive : ""}`}
-               to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
-      <NavLink className={({isActive}) => `${s.nav__item} ${isActive ? s.nav__itemActive : ""}`}
-               to={PATH.JUNIOR}>Junior</NavLink>
-      <NavLink className={({isActive}) => `${s.nav__item} ${isActive ? s.nav__itemActive : ""}`}
-               to={PATH.JUNIOR_PLUS}>Junior-Plus</NavLink>
-    </nav>
+    <div className={s.headerContainer}>
+      <SuperButton
+        onClick={onBtnHandler}
+        className={s.menuBtn}>
+        Menu
+      </SuperButton>
+      {
+        menu
+          ? <nav className={hideMenuClass}></nav>
+          : <nav className={hideMenuClass}>
+            <NavLink className={({isActive}) => `${s.nav__item} ${isActive ? s.nav__itemActive : ""}`}
+                     to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
+            <NavLink className={({isActive}) => `${s.nav__item} ${isActive ? s.nav__itemActive : ""}`}
+                     to={PATH.JUNIOR}>Junior</NavLink>
+            <NavLink className={({isActive}) => `${s.nav__item} ${isActive ? s.nav__itemActive : ""}`}
+                     to={PATH.JUNIOR_PLUS}>Junior-Plus</NavLink>
+          </nav>
+      }
+    </div>
+
   )
 }
 
