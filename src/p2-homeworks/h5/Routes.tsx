662641891169c7b3be39b6600d9521f25aab56c1
Ivan <adamovskij00@mail.ru>
2021-12-21 23:12:50 +0300

    add default Route

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -20,7 +20,7 @@ function RoutesContainer() {
 
         {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
         {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-        <Route path={'/'} element={PATH.PRE_JUNIOR}/>
+        <Route path={'/'} element={<PreJunior/>}/>
         <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
         <Route path={PATH.JUNIOR} element={<Junior/>}/>
         <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
