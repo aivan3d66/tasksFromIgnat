04e216a20d6f0201695a75a4a1a84cd6cd3c570b
Ivan <adamovskij00@mail.ru>
2021-12-21 22:51:59 +0300

    add useState

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './Header'
 import RoutesContainer from './Routes'
 
+export type OnBtnHandler = () => void;
+
 function HW5() {
+  const [menu, setMenu] = useState<boolean>(true)
+  const onBtnHandler: OnBtnHandler = () => setMenu(!menu);
+
   return (
     <div>
-      <Header/>
+      <Header menu={menu} onBtnHandler={onBtnHandler}/>
       <RoutesContainer/>
     </div>
   )
