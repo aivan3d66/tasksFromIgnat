0c2fac1e9eae547026a206af1c80dc58d38e1714
Ivan <adamovskij00@mail.ru>
2021-12-21 21:17:11 +0300

    add navigation with Navlink

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
+import {PATH} from "./Routes";
+
+const s = require('./HW5.module.css');
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
-
-        </div>
-    )
+  return (
+    <nav className={s.navList}>
+      <NavLink className={({isActive}) => `${s.nav__item} ${isActive ? s.nav__itemActive : ""}`}
+               to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
+      <NavLink className={({isActive}) => `${s.nav__item} ${isActive ? s.nav__itemActive : ""}`}
+               to={PATH.JUNIOR}>Junior</NavLink>
+      <NavLink className={({isActive}) => `${s.nav__item} ${isActive ? s.nav__itemActive : ""}`}
+               to={PATH.JUNIOR_PLUS}>Junior-Plus</NavLink>
+    </nav>
+  )
 }
 
 export default Header
