14d13e3721cb70ae5caf6aa341360a8ade91305f
Ivan <adamovskij00@mail.ru>
2021-12-22 18:31:34 +0300

    add onChangeCallback logic, types and radio attributes

--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -1,40 +1,48 @@
 import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
 
 type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
-
 type SuperRadioPropsType = DefaultRadioPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+  options?: Array<string>,
+  onChangeOption?: (option: string) => void
 }
 
 const SuperRadio: React.FC<SuperRadioPropsType> = (
-    {
-        type, name,
-        options, value,
-        onChange, onChangeOption,
-        ...restProps
-    }
+  {
+    type,
+    name,
+    options,
+    value,
+    onChange,
+    onChangeOption,
+    checked,
+    ...restProps
+  }
 ) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // onChange, onChangeOption
-    }
+  const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    onChangeOption && onChangeOption(e.currentTarget.value)
+    onChange && onChange(e)
+  }
 
 
-    const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
-        <label key={name + '-' + i}>
-            <input
-                type={'radio'}
-                // name, checked, value, onChange
-            />
-            {o}
-        </label>
-    )) : []
+  const mappedOptions: DefaultRadioPropsType[] = options ? options.map((o, i) => (
+    <label key={name + '-' + i}>
+      <input
+        type={'radio'}
+        name={name}
+        value={value}
+        checked={checked}
+        onChange={onChangeCallback}
+        {...restProps}
+      />
+      {o}
+    </label>
+  )) : []
 
-    return (
-        <>
-            {mappedOptions}
-        </>
-    )
+  return (
+    <>
+      {mappedOptions}
+    </>
+  )
 }
 
 export default SuperRadio
