c3f88b30306c5271b6345a424630e0ec21344f27
Ivan <adamovskij00@mail.ru>
2021-12-22 09:25:40 +0300

    add default route

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -6,29 +6,23 @@ import {Junior} from "./pages/Junior";
 import {JuniorPlus} from "./pages/JuniorPlus";
 
 export const PATH = {
+  DEFAULT_ROUTE: '/',
   PRE_JUNIOR: '/pre-junior',
   JUNIOR: '/junior',
   JUNIOR_PLUS: '/junior-plus',
-  // add paths
 }
 
 function RoutesContainer() {
   return (
     <div>
-      {/*Switch выбирает первый подходящий роут*/}
       <Routes>
-
-        {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-        {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-        <Route path={'/'} element={<PreJunior/>}/>
+        <Route path={PATH.DEFAULT_ROUTE} element={<PreJunior/>}/>
         <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
         <Route path={PATH.JUNIOR} element={<Junior/>}/>
         <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
-        {/*add routes*/}
 
         {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
         <Route element={<Error404/>}/>
-
       </Routes>
     </div>
   )
