f964560349bedbfae833cea1d789ee85a2294783
Ivan <adamovskij00@mail.ru>
2021-12-22 10:49:33 +0300

    add some error styles

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+const s = require('./../HW5.module.css');
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
-        </div>
+      <div className={s.error}>
+        <div className={s.errorCode}>404</div>
+        <div className={s.errorMessage}>Page not found!</div>
+        <div className={s.errorImg}>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+      </div>
     )
 }
 
