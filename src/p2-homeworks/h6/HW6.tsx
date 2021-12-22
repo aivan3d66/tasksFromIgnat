222232ab8ef6b5738058e40103b62b67b07de797
Ivan <adamovskij00@mail.ru>
2021-12-22 16:14:40 +0300

    add h2 tag and div containers

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -3,38 +3,40 @@ import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
 
+const s = require('./HW6.module.css');
+
 function HW6() {
-    const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<string>('')
 
-    const save = () => {
-        saveState<string>('editable-span-value', value)
-    }
-    const restore = () => {
-        // setValue()
-    }
+  const save = () => {
+    saveState<string>('editable-span-value', value)
+  }
+  const restore = () => {
+    let valueFromStore = restoreState('editable-span-value', value);
+    setValue(`${valueFromStore}`)
+  }
 
-    return (
-        <div>
-            <hr/>
-            homeworks 6
+  return (
+    <div className={s.contentWrapper}>
+      <h2>Homeworks 6</h2>
 
-            {/*should work (должно работать)*/}
-            <div>
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
-                />
-            </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
+      {/*should work (должно работать)*/}
+      <div className={s.inputWrapper}>
+        <SuperEditableSpan
+          value={value}
+          onChangeText={setValue}
+          spanProps={{children: value ? undefined : 'enter text...'}}
+        />
+      </div>
+      <div className={s.btnWrapper}>
+        <SuperButton onClick={save}>save</SuperButton>
+        <SuperButton onClick={restore}>restore</SuperButton>
+      </div>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperEditableSpan/>*/}
-            <hr/>
-        </div>
-    )
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperEditableSpan/>*/}
+    </div>
+  )
 }
 
 export default HW6
