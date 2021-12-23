7ca4e47512fd7cdfe8db5b0a5de90f5ac6e9afe0
Ivan <adamovskij00@mail.ru>
2021-12-23 23:00:16 +0300

    add unit test for sortUp

--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -15,10 +15,17 @@ beforeEach(() => {
 })
 
 test('sort name up', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
-
-    console.log(newState)
-    // expect(...).toBe(...)
+  const newState = homeWorkReducer(initialState, {type: FILTER.SORT, payload: ACTION_PAYLOAD.UP})
+  expect(newState).toEqual(
+    [
+      {_id: 0, name: 'Кот', age: 3},
+      {_id: 2, name: 'Коля', age: 16},
+      {_id: 5, name: 'Ирина', age: 55},
+      {_id: 4, name: 'Дмитрий', age: 40},
+      {_id: 3, name: 'Виктор', age: 44},
+      {_id: 1, name: 'Александр', age: 66},
+    ]
+  )
 })
 test('sort name down', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
