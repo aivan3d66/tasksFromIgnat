ef847c16ba898310e7948f6581872c2bae70bc52
Ivan <adamovskij00@mail.ru>
2021-12-23 23:04:26 +0300

    add sortUp, sortDown, sortMin functions with typing

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,48 +1,70 @@
 import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
+import {ACTION_PAYLOAD, FILTER, homeWorkReducer} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
-// export type UserType =
+const s = require('./HW8.module.css');
 
-const initialPeople = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+export type UserType = {
+  _id: number,
+  name: string,
+  age: number,
+}
+
+const initialPeople: Array<UserType> = [
+  {_id: 0, name: 'Кот', age: 3},
+  {_id: 1, name: 'Александр', age: 66},
+  {_id: 2, name: 'Коля', age: 16},
+  {_id: 3, name: 'Виктор', age: 44},
+  {_id: 4, name: 'Дмитрий', age: 40},
+  {_id: 5, name: 'Ирина', age: 55},
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
+  const [people, setPeople] = useState<Array<UserType>>(initialPeople) // need to fix any
 
-    // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
-        </div>
-    ))
+  const finalPeople = people.map((p: UserType) => (
+    <div key={p._id}>
+      {p.name}, {p.age}
+    </div>
+  ))
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+  const sortUp = () => setPeople(homeWorkReducer(initialPeople, {
+    type: FILTER.SORT,
+    payload: ACTION_PAYLOAD.UP
+  }))
 
-    return (
+  const sortDown = () => setPeople(homeWorkReducer(initialPeople, {
+    type: FILTER.SORT,
+    payload: ACTION_PAYLOAD.DOWN
+  }))
+
+  const sortMin = () => setPeople(homeWorkReducer(initialPeople, {
+    type: FILTER.CHECK,
+    payload: 18
+  }))
+
+  console.log( people )
+
+  return (
+    <div className={s.contentWrapper}>
+      <h2>Homeworks 8</h2>
+      {finalPeople}
+
+      <div>
         <div>
-            <hr/>
-            homeworks 8
-
-            {/*should work (должно работать)*/}
-            {finalPeople}
-
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativePeople/>*/}
-            <hr/>
+          <SuperButton onClick={sortUp}>sort up</SuperButton>
         </div>
-    )
+        <div>
+          <SuperButton onClick={sortDown}>sort down</SuperButton>
+        </div>
+        <div>
+          <SuperButton onClick={sortMin}>check 18</SuperButton>
+        </div>
+      </div>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativePeople/>*/}
+    </div>
+  )
 }
 
 export default HW8
