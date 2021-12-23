5afe6d4908f0e4550701acc3b906cadc1d05e832
Ivan <adamovskij00@mail.ru>
2021-12-23 23:01:24 +0300

    add homeWorkReducer with actions, clean functions and typing

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,46 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
+import {UserType} from "../HW8";
+
+type FiltersType = { SORT: string, CHECK: string }
+type ActionPayloadType = { UP: string, DOWN: string, NUMBER: number}
+type ActionType = {type: string, payload: number | string}
+type SortDown = (a: UserType, b: UserType) => number;
+type SortUp = (a: UserType, b: UserType) => number;
+
+export const FILTER: FiltersType = {
+  SORT: 'SORT',
+  CHECK: 'CHECK',
+}
+
+export const ACTION_PAYLOAD: ActionPayloadType = {
+  UP: 'UP',
+  DOWN: 'DOWN',
+  NUMBER: 18,
+}
+
+const sortDown: SortDown = (a, b) => {
+  if (a.name < b.name) { return -1; }
+  if (a.name > b.name) { return 1; }
+  return 0;
+}
+
+const sortUp: SortUp = (a, b) => {
+  if (a.name > b.name) { return -1; }
+  if (a.name < b.name) { return 1; }
+  return 0;
+}
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): any => { // need to fix any
+  switch (action.type && action.payload) {
+    case FILTER.SORT && ACTION_PAYLOAD.DOWN: {
+      return [...state.sort(sortDown)]
     }
+    case FILTER.SORT && ACTION_PAYLOAD.UP: {
+      return [...state.sort(sortUp)]
+    }
+    case FILTER.CHECK && ACTION_PAYLOAD.NUMBER: {
+      return state.filter(item => item.age >= action.payload);
+    }
+    default:
+      return state
+  }
 }
