14f5dc19a79f8fffc806b216519ba2a0b21f941a
Ivan <adamovskij00@mail.ru>
2021-12-24 12:02:36 +0300

    add contentWrapper and h2 title

--- src/p2-homeworks/h9/HW9.tsx
+++ src/p2-homeworks/h9/HW9.tsx
@@ -1,21 +1,17 @@
 import React from 'react'
 import Clock from './Clock'
 
+const s = require('./HW9.module.css')
+
 function HW9() {
-    return (
-        <div>
-            <hr/>
-            homeworks 9
-
-            {/*should work (должно работать)*/}
-            <Clock/>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeClock/>*/}
-            <hr/>
-        </div>
-    )
+  return (
+    <div className={s.contentWrapper}>
+      <h2>Homeworks 9</h2>
+      <Clock/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeClock/>*/}
+    </div>
+  )
 }
 
 export default HW9
