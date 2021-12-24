84c4cc82daa6ce7ee9c51a028d3f2321c15316bd
Ivan <adamovskij00@mail.ru>
2021-12-24 23:02:02 +0300

    add loadingReducer import

--- src/p1-main/m1-ui/u1-app/redux/redux-store.tsx
+++ src/p1-main/m1-ui/u1-app/redux/redux-store.tsx
@@ -1,8 +1,10 @@
 import {createStore, combineReducers} from "redux";
 import {messagesReducer} from "./reducer/DialogReducer";
+import {loadingReducer} from "../../../../p2-homeworks/h10/bll/loadingReducer";
 
 const reducers = combineReducers({
   messagePage: messagesReducer,
+  loading: loadingReducer,
 });
 
 const store = createStore(reducers);
