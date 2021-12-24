621f9041b06d1676429330c543e1e3536b985473
Ivan <adamovskij00@mail.ru>
2021-12-24 22:59:33 +0300

    add PreloaderContainer import as HW10

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
 import HW9 from "../../h9/HW9";
+import PreloaderContainer from "../../h10/PreloaderContainer";
 
 export const Junior = () => {
   return (
@@ -12,6 +13,8 @@ export const Junior = () => {
       <HW8/>
       <hr/>
       <HW9/>
+      <hr/>
+      <PreloaderContainer/>
       <hr/>
     </div>
   )
