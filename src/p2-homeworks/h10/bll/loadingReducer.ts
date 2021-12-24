39b99a2f3ab2b49d64014dcf746aeec0a5f484ce
Ivan <adamovskij00@mail.ru>
2021-12-24 23:00:15 +0300

    add action obj

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -4,6 +4,13 @@ export type ActionReducer = {type: string}
 export type LoadingAC = () => ActionReducer;
 export type LoadingStopAC = () => ActionReducer;
 
+const ACTION: ActionObj = {
+  IS_LOADING: "IS-LOADING",
+  STOP_LOADING: "STOP_LOADING"
+}
+
+const initState: InitialState = {
+  isLoading: false,
 }
 
 export const loadingReducer = (state = initState, action: any): any => { // fix any
