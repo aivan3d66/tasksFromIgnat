094625cbd6395178f082e237d2ef5ead9eab0373
Ivan <adamovskij00@mail.ru>
2022-01-10 21:47:14 +0300

    add HW11 import

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -3,6 +3,7 @@ import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
 import HW9 from "../../h9/HW9";
 import PreloaderContainer from "../../h10/PreloaderContainer";
+import HW11 from "../../h11/HW11";
 
 export const Junior = () => {
   return (
@@ -15,6 +16,8 @@ export const Junior = () => {
       <HW9/>
       <hr/>
       <PreloaderContainer/>
+      <hr/>
+      <HW11/>
       <hr/>
     </div>
   )
