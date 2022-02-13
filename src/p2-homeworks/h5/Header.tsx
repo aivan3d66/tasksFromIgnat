e5a64bef54bf8a7a7de8ec3f1b7027579138f9c0
Ivan <adamovskij00@mail.ru>
2022-02-13 21:35:43 +0300

    add MenuRoundedIcon

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton';
 import {PATH} from "./Routes";
 import {OnBtnHandler} from "./HW5";
-
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 const s = require('./HW5.module.css');
 
 type HeaderProps = {
@@ -20,7 +20,7 @@ const Header: React.FC<HeaderProps> = ({menu, onBtnHandler}) => {
       <SuperButton
         onClick={onBtnHandler}
         className={s.menuBtn}>
-        &#9658;
+        <MenuRoundedIcon/>
       </SuperButton>
       {
         menu
