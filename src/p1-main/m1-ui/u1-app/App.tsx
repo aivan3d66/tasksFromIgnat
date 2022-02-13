42f9dfb82068f3c84dc9e90926b55870b0bcaf16
Ivan <adamovskij00@mail.ru>
2022-02-13 21:35:33 +0300

    add upd App main page styles

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,13 +1,53 @@
 import React from 'react';
 import s from './App.module.css';
 import HW5 from '../../../p2-homeworks/h5/HW5';
-
+import {AppBar, Container, Grid, IconButton, Toolbar, Typography} from '@mui/material';
+import {useSelector} from "react-redux";
+import {AppRootState} from "./redux/redux-store";
 
 function App() {
+  const theme = useSelector<AppRootState, any>(state => state.theme.scheme);
+
+
+  const appStyles = {
+    headerColour: `${theme.headerColour}`,
+    appBackgroundColour: `${theme.appBackgroundColour}`,
+    textColour: `${theme.textColour}`
+  }
+
   return (
     <div className={s.App}>
-      <h1>React homeworks:</h1>
-      <HW5/>
+      <div className="App" style={{backgroundColor: `${appStyles.appBackgroundColour}`}}>
+        <AppBar position="static" style={{backgroundColor: `${appStyles.headerColour}`}}>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+
+              aria-label="menu"
+              sx={{mr: 2}}
+            >
+              {/*<MenuIcon/>*/}
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+              React homeworks
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Container fixed maxWidth="xl">
+          <Grid container spacing={2} style={{
+            display: "flex",
+            justifyContent: "center",
+            width: "100%",
+            margin: "40px 0 0 0",
+            borderRadius: "5px",
+            color: `${appStyles.textColour}`
+          }}>
+            <HW5/>
+          </Grid>
+        </Container>
+      </div>
     </div>
   )
 }
