b25afc1f662854784f1ad0148311bdfa4a77a0bc
Ivan <adamovskij00@mail.ru>
2022-02-13 21:36:48 +0300

    add HW12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,26 +1,50 @@
 import React from "react";
-import s from "./HW12.module.css";
+import {useDispatch} from "react-redux";
+import {
+  darkScheme,
+  setDarkTheme,
+  setWhiteTheme,
+  whiteScheme
+} from "../../p1-main/m1-ui/u1-app/redux/reducer/themeReducer";
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import ModeNightRoundedIcon from '@mui/icons-material/ModeNightRounded';
+import WbSunnyRoundedIcon from '@mui/icons-material/WbSunnyRounded';
+import {IconButton} from "@mui/material";
 
-const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
+  const dispatch = useDispatch();
 
-    // useDispatch, onChangeCallback
+  const onChangeDarkThemeHandler = () => {
+    dispatch(setDarkTheme(darkScheme))
+  }
+  const onChangeLightThemeHandler = () => {
+    dispatch(setWhiteTheme(whiteScheme))
+  }
 
-    return (
-        <div className={s[theme]}>
-            <hr/>
-            <span className={s[theme + '-text']}>
-                homeworks 12
-            </span>
-
-            {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
-
-            <hr/>
+  return (
+    <div>
+      <h2>Homeworks 12</h2>
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+      }}>
+        <div style={{
+          backgroundColor: "rgba(169, 169, 169, 1)",
+          border: "1px solid white",
+          borderRadius: "50px",
+        }}>
+          <IconButton onClick={onChangeLightThemeHandler}>
+            <WbSunnyRoundedIcon style={{color: "yellow"}}/>
+          </IconButton>
+          <IconButton onClick={onChangeDarkThemeHandler}>
+            <ModeNightRoundedIcon style={{color: "gold"}}/>
+          </IconButton>
         </div>
-    );
+      </div>
+      <hr/>
+    </div>
+  );
 }
 
 export default HW12;
