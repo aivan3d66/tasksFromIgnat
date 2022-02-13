d456c8bffe7cfe661cd15e0973acb86071b6b44b
Ivan <adamovskij00@mail.ru>
2022-02-13 21:34:14 +0300

    add themeReducer to rootReducer

--- src/p1-main/m1-ui/u1-app/redux/redux-store.tsx
+++ src/p1-main/m1-ui/u1-app/redux/redux-store.tsx
@@ -1,12 +1,16 @@
 import {createStore, combineReducers} from "redux";
 import {messagesReducer} from "./reducer/messageReducer";
 import {loadingReducer} from "./reducer/loadingReducer";
+import {themeReducer} from "./reducer/themeReducer";
 
-const reducers = combineReducers({
+export type AppRootState = ReturnType<typeof rootReducer>
+
+const rootReducer = combineReducers({
   messagePage: messagesReducer,
   loading: loadingReducer,
+  theme: themeReducer
 });
 
-const store = createStore(reducers);
+const store = createStore(rootReducer);
 
 export default store;
