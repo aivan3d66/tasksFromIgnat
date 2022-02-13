cc5679483d040335b479d85485587025acdf29cb
Ivan <adamovskij00@mail.ru>
2022-02-13 21:34:57 +0300

    add themeReducer logic

--- src/p1-main/m1-ui/u1-app/redux/reducer/themeReducer.tsx
+++ src/p1-main/m1-ui/u1-app/redux/reducer/themeReducer.tsx
@@ -1,25 +1,48 @@
 const SET_WHITE_THEME = 'SET_WHITE_THEME';
 const SET_DARK_THEME = 'SET_DARK_THEME';
 
-export const themeReducer = (state: any, action: any) => {
+export const whiteScheme = {
+  headerColour: 'rgba(65, 105, 225, 1)',
+  appBackgroundColour: 'rgba(220, 220, 220, 0.5)',
+  textColour: 'black'
+}
+export const darkScheme = {
+  headerColour: 'rgba(44, 51, 59, 1)',
+  appBackgroundColour: 'rgba(34, 39, 45, 1)',
+  textColour: 'rgba(147, 167, 218, 1)',
+}
+
+const initialState = {
+  scheme: whiteScheme
+}
+
+export const themeReducer = (state: any = initialState, action: any) => {
   switch (action.type) {
     case SET_WHITE_THEME: {
       return {
-        ...state
+        ...state,
+        scheme: action.payload
       }
     }
 
     case SET_DARK_THEME: {
       return {
-        ...state
+        ...state,
+        scheme: action.payload
       }
+    }
+
+    default: {
+      return state;
     }
   }
 }
 
-const setWhiteTheme = () => ({
-  type: SET_WHITE_THEME
+export const setWhiteTheme = (whiteScheme: any) => ({
+  type: SET_WHITE_THEME,
+  payload: whiteScheme
 })
-const setDarkTheme = () => ({
-  type: SET_DARK_THEME
+export const setDarkTheme = (darkScheme: any) => ({
+  type: SET_DARK_THEME,
+  payload: darkScheme
 })
