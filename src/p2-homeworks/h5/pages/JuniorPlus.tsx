d034f8706a0544030d3e82e64c97444570609f35
Ivan <adamovskij00@mail.ru>
2022-02-25 22:15:27 +0300

    add JuniorPlus styles and hr tags

--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -3,8 +3,14 @@ import HW12 from "../../h12/HW12";
 
 export const JuniorPlus = () => {
   return (
-    <div>
+    <div style={{
+      width: "500px",
+      margin: "0 auto",
+      padding: "0",
+    }}>
+      <hr/>
       <HW12/>
+      <hr/>
     </div>
   )
 }
