e8b2b809f507782dd464e26ee975c999726eb35d
Ivan <adamovskij00@mail.ru>
2022-03-03 09:48:13 +0300

    add onCheckboxHandler and checked useState

--- src/p2-homeworks/h13/Request.tsx
+++ src/p2-homeworks/h13/Request.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 import SuperCheckbox from "../h4/common/c3-SuperCheckbox/SuperCheckbox";
 
 function Request() {
+  const [checked, setChecked] = useState<boolean>(false);
+
+  const onCheckboxHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    return setChecked(e.currentTarget.checked)
+  }
+
   return (
     <div>
       <div>
@@ -11,7 +17,10 @@ function Request() {
         </SuperButton>
       </div>
       <div>
-        <SuperCheckbox>
+        <SuperCheckbox
+          checked={checked}
+          onChange={onCheckboxHandler}
+        >
           check
         </SuperCheckbox>
       </div>
