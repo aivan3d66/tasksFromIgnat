bf380a13b6e8ce11efa983f2bfd4fad23143cead
Ivan <adamovskij00@mail.ru>
2022-03-03 11:51:38 +0300

    add onButtonClickHandler

--- src/p2-homeworks/h13/Request.tsx
+++ src/p2-homeworks/h13/Request.tsx
@@ -6,16 +6,23 @@ function Request() {
   const [checked, setChecked] = useState<boolean>(false);
   const [state, setState] = useState<string>('');
 
-  console.log(state)
-
   const onCheckboxHandler = (e: ChangeEvent<HTMLInputElement>) => {
     return setChecked(e.currentTarget.checked)
+  }
+  const onButtonClickHandler = () => {
+    // return postData(checked)
+    //   .then(res => setState(res))
+    //   .catch(error => setState(error.response ? error.response.data.errorText : error.message))
+
+    return axiosPostData.postRequest(checked)
+      .then(res => setState(res.data))
+      .catch(error => setState(error.response ? error.response.data.errorText : error.message))
   }
 
   return (
     <div>
       <div>
-        <SuperButton>
+        <SuperButton onClick={onButtonClickHandler}>
           click for request
         </SuperButton>
       </div>
