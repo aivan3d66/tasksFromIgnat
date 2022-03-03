6e1553d5585e7daeb62e38c5b0a500cb89f66f80
Ivan <adamovskij00@mail.ru>
2022-03-03 09:15:04 +0300

    add HW13 import

---
+++ src/p2-homeworks/h13/HW13.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function HW13() {
+
+  return (
+    <div style={{padding: "0 20px"}}>
+      <h2>Homeworks 13</h2>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default HW13;
